73594f9ce084ddbe1057c29f72c32edf2e9c9aa3
tuhinangshusingha <tuhinangshusingha@gmail.com>
2025-08-16 13:43:05 +0530

    Add files via upload

---
+++ t1/g2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	float num1,num2,s;
+	printf("Enter two floating nos:");
+	scanf("%f %f",&num1,&num2);
+	s=num1+num2;
+	printf("%f+%f=%f",num1,num2,s);
+	return 0;
+}
