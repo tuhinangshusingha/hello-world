21b8545ed5ba91a92b37f4f0c02fec12ef573b6d
tuhinangshusingha <tuhinangshusingha@gmail.com>
2025-08-16 14:25:08 +0530

    Add files via upload

---
+++ github3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter a no:");
+	scanf("%d",&n);
+	printf("%dx1=%d",n,n*1);
+	printf("%dx2=%d",n,n*2);
+	printf("%dx3=%d",n,n*3);
+	printf("%dx4=%d",n,n*4);
+	printf("%dx5=%d",n,n*5);
+	printf("%dx6=%d",n,n*6);
+	printf("%dx7=%d",n,n*7);
+	printf("%dx8=%d",n,n*8);
+	printf("%dx9=%d",n,n*9);
+	printf("%dx10=%d",n,n*10);
+}
